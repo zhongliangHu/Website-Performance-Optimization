8dab10fb0d4ab0198cff2082777b4053eaf5d765
huzhongliang <huzl0525@163.com>
2017-04-26 16:33:29 +0800

    compress imgs

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+gulp.task('images', function(){
+  return gulp.src('images/**/*.+(png|jpg|jpeg|gif|svg|)')
+    .pipe(imagemin({
+       interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+       optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+       progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+       multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+     }))
+    .pipe(gulp.dest('img'));
+});
+
+//在命令行写入< gulp auto启动监听任务
+gulp.task('auto', function(){
+    gulp.watch('images/**/*.+(png|jpg|jpeg|gif|svg|)',['images']);
+});
+
+//使用gulp.task('default')定义默认任务
+//在命令行输入< gulp 启动images任务和auto任务
+gulp.task('default',['images','auto']);
